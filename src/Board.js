5c9fb0996f122b7718c87276474169c3e6dca571
justin <justindulark@gmail.com>
2017-12-06 11:31:50 -0800

    Implement code for checking row conflicts
    hasRowConflictAt checks single row, and counts pieces in the row and
    returns true if there is more than one. hasAnyRowConflicts checks all
    rows using hasRowConflictAt and returns true if any rows have a
    conflict.

--- src/Board.js
+++ src/Board.js
@@ -62,14 +62,14 @@
     },
 
 
-/*
+    /*
          _             _     _
      ___| |_ __ _ _ __| |_  | |__   ___ _ __ ___ _
     / __| __/ _` | '__| __| | '_ \ / _ \ '__/ _ (_)
     \__ \ || (_| | |  | |_  | | | |  __/ | |  __/_
     |___/\__\__,_|_|   \__| |_| |_|\___|_|  \___(_)
 
- */
+    */
     /*=========================================================================
     =                 TODO: fill in these Helper Functions                    =
     =========================================================================*/
@@ -79,12 +79,28 @@
     //
     // test if a specific row on this board contains a conflict
     hasRowConflictAt: function(rowIndex) {
-      return false; // fixme
-    },
+      var numPieces = 0;
+      var testRow = this.get(rowIndex);
+      for (var x = 0; x < testRow.length; x++) {
+        if (testRow[x] === 1) {
+          numPieces++;
+        }
+      }
+      if (numPieces > 1) {
+        return true;
+      } else {
+        return false;
+      }
+    },  
 
     // test if any rows on this board contain conflicts
     hasAnyRowConflicts: function() {
-      return false; // fixme
+      for (var x in this.attributes) {
+        if (this.hasRowConflictAt(x) === true) {
+          return true;
+        }
+      }
+      return false;
     },
 
 
